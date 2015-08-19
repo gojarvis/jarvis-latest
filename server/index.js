edd75e85504bd447bb6e73d590d313ed17a662df
parties <eric.m.ogden@gmail.com>
2015-08-19 02:37:54 -0700

    socket.io server, opens on port 3000, waits for events and echoes incoming responses

---
+++ server/index.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var port = process.env.PORT || 3000;
+
+server.listen(port, function () {
+  console.log('Server listening at port %d', port);
+});
+
+// Routing
+app.use(express.static(__dirname + '/public'));
+
+// Chatroom
+
+// usernames which are currently connected to the chat
+var numUsers = 0;
+
+io.on('connection', function (socket) {
+  console.log('got connection:', socket.id);
+  numUsers++;
+  socket.emit('login', {
+    test: 'yay!'
+  });
+
+  socket.on('file opened', function(file) {
+    console.log('file opened:', file)
+  })
+
+  socket.on('disconnect', function() {
+    console.log('disconnected')
+    numUsers--;
+  })
+})
