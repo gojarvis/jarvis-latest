b4ecf09af7d78088914514142c2c791d9741c2da
parties <eric.m.ogden@gmail.com>
2015-08-22 01:48:00 -0700

    added moniker names to connections

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var port = process.env.PORT || 3000;
 var imm = require('immutable');
+var moniker = require('moniker');
+var names = moniker.generator([moniker.adjective, moniker.noun]);
 
 server.listen(port, function () {
   console.log('Server listening at port %d', port);
@@ -16,15 +18,23 @@ app.use(express.static(__dirname + '/public'));
 
 // usernames which are currently connected to the chat
 var numUsers = 0;
+var connections = imm.Set();
 var files = imm.Set();
 
 io.on('connection', function (socket) {
-  console.log('got connection:', socket.id);
-  numUsers++;
-  socket.emit('login', {
-    test: 'yay!'
-  });
+  var newConn = {
+    id: socket.id,
+    name: names.choose()
+  };
+  connections = connections.add(newConn);
+  socket.emit('name', newConn);
+  console.log('connections: ', connections.toJS());
+
   socket.emit('files', files)
+
+  socket.on('files', function(files) {
+    console.log('files:', files);
+  });
 
   socket.on('file opened', function(file) {
     console.log('file opened:', file)
@@ -42,8 +52,18 @@ io.on('connection', function (socket) {
     console.log('file saved:', file)
   });
 
+  socket.on('cmd', function(cmd) {
+    console.log('cmd:', cmd);
+  });
+
   socket.on('disconnect', function() {
+    connections = connections.filter(function(item) {
+      if(item.id !== socket.id) {
+        return true;
+      }
+    });
+
     console.log('disconnected')
     numUsers--;
-  })
+  });
 })
