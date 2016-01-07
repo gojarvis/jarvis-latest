18e2f1245bb393458e4fe11a8a4e47af6c5e4fa9
Roie Cohen <roie.cohen@gmail.com>
2016-01-06 20:59:10 -0800

    playing with ifttt

--- server/src/main.js
+++ server/src/main.js
@@ -1,15 +1,35 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var bodyParser = require('body-parser');
 
 import SocketManager from './utils/socket-manager';
+
+
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function(req, res){
   res.sendfile('client/src/www/index.html');
 });
 
+
+
 io.on('connection', function(socket){
   var socketManager = new SocketManager(socket,io)
+
+  app.get('/alexa', function(req,res){
+    io.emit('speak', "AAlexa");
+    io.emit('speak', "");
+    io.emit('speak', "Trigger Jarvis Greeting");
+  })
+
+  app.post('/phone', function(req,res){
+    io.emit('speak', "Hello Phone");
+    console.log('phone', req.body);
+  })
 });
 
 
