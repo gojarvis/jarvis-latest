8e6ae3d1a9fb5bed8ad527ce9b23d2cdae03f878
Roie Cohen <roie.cohen@gmail.com>
2016-03-09 12:56:18 -0800

    finally socket cluster and es7

--- server/src/init.js
+++ server/src/init.js
@@ -1,3 +1,8 @@
 module.exports.run = function(thisWorker) {
-    require("babel-register");
+    require("babel-register")({
+        "presets": ["es2015", "stage-0"],
+        plugins: ["syntax-async-generators", "syntax-async-functions"]
+    });
+  require("babel-polyfill");
+
 }
