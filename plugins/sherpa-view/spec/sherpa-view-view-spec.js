39c36c9625038ec27de0cb51f8d8042bf474b822
Roie Cohen <roie.cohen@gmail.com>
2016-07-24 17:42:20 -0700

    playing around with atom

---
+++ plugins/sherpa-view/spec/sherpa-view-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import SherpaViewView from '../lib/sherpa-view-view';
+
+describe('SherpaViewView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
