d80bea1a62735fd321e137854677a63bc0be17c4
Eric Ogden <eric.m.ogden@gmail.com>
2016-08-02 23:25:20 -0700

    small style fix

--- client/src/app/components/EventTicker/EventTickerItem.jsx
+++ client/src/app/components/EventTicker/EventTickerItem.jsx
@@ -67,7 +67,7 @@ class EventTickerItem extends React.Component {
 
 
     return (
-      <div style={{flex: "1 auto"}}>
+      <div>
         <div style={{...this.props.style, ...STYLES.container}} onClick={() => this.props.onClick(this.props.item.data.nodeId)}>
           <Card zDepth={4} style={{height: "120px", minWidth: "220px"}}>
              <CardText style={{...FB.base, flexDirection: 'column', display: "flex", justifyContent: "space-between"}}>
