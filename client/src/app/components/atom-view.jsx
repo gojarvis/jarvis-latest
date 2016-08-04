9913b5477ab5c1166252c8c6a2b5ec6d85c80c52
Eric Ogden <eric.m.ogden@gmail.com>
2016-08-03 18:14:16 -0700

    user list buttons + click handlers

--- client/src/app/components/atom-view.jsx
+++ client/src/app/components/atom-view.jsx
@@ -15,6 +15,7 @@ import IconText from './IconText';
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/lib/card';
 import RaisedButton from 'material-ui/lib/raised-button';
 import CSSTransitionGroup from 'react-addons-css-transition-group';
+import UserList from 'components/UserList';
 require('./QueriedItems.css');
 
 
@@ -65,7 +66,7 @@ class AtomView extends React.Component {
 
   async handleFilter(eventType){
     let nodeId = this.state.items[0].startNode.id;
-    let params = {nodeId:nodeId};
+    let params = {nodeId};
     let type;
     switch (eventType){
       case 'Files':
@@ -84,6 +85,15 @@ class AtomView extends React.Component {
     this.setState({
       items: result.body
     })
+  }
+
+  async handleUserFilter(user) {
+    let params = {users: [user]};
+    let result = await agent.post('http://localhost:3000/query', params);
+
+    this.setState({
+      items: result.body
+    });
   }
 
   async externalLinkClick(address, type){
@@ -137,6 +147,7 @@ class AtomView extends React.Component {
       )
     }
     let filters = ['All', 'Files', 'URLs', 'Keywords'];
+    let users = [{username: 'parties', id: '1'}, {username: 'roieki', id: '2'}];
 
     return(
       <div style={{width: "100%"}}>
@@ -159,10 +170,12 @@ class AtomView extends React.Component {
                     label={filter}
                     primary={index === 1}
                     secondary={index === 2}
-                    onClick={()=>this.handleFilter(filter)} />
+                    onClick={()=>this.handleFilter(filter)}
+                    style={{flex: '1 1 auto', margin: 10}} />
                 )
               })}
             </div>
+            <UserList users={users} onClick={this.handleUserFilter} />
             <hr />
               <CSSTransitionGroup
                 transitionName='query-item'
