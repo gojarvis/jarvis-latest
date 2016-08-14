de49cff3f558dcd88037b2013adaa8c11e15fbca
Roie Cohen <roie.cohen@gmail.com>
2016-08-13 21:12:40 -0700

    small updates

--- start.sh
+++ start.sh
@@ -1,17 +1,15 @@
 #!/bin/sh
-echo "====================================" >> /var/log/Jarvis/roie.log
-echo "====================================" >> /var/log/Jarvis/roie.log
-echo "====================================" >> /var/log/Jarvis/roie.log
-echo "====================================" >> /var/log/Jarvis/roie.log
+echo "===================================="
+echo "===================================="
+echo "===================================="
+echo "===================================="
 
 
-# less `pwd`/node_modules/pm2/bin/pm2 >> /var/log/Jarvis/roie.log
-echo `date`"TEST JARVIS" >> /var/log/Jarvis/roie.log
-cd `pwd`/client/
-echo `pwd` >> /var/log/Jarvis/roie.log
-npm start &
+# less `pwd`/node_modules/pm2/bin/pm2
+echo `date`"TEST JARVIS"
+# npm install --save express
+`pwd`/node_modules/pm2/bin/pm2 status
+`pwd`/node_modules/pm2/bin/pm2 update
+`pwd`/node_modules/pm2/bin/pm2 start `pwd`/sherpa.json
 
-cd ../server/
-echo `pwd` >> /var/log/Jarvis/roie.log
-npm run nodemon &
-echo `date`"AFTER LAUNCHING" >> /var/log/Jarvis/roie.log
+echo `date`"AFTER LAUNCHING"
