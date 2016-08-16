44d13242b36027f20c59fc20afff9aa24b2bbb43
Roie Cohen <roie.cohen@gmail.com>
2016-08-16 11:35:41 -0700

    removed logs and sockets

--- plugins/schrome/app/todoapp/containers/Root.js
+++ plugins/schrome/app/todoapp/containers/Root.js
@@ -1,8 +1,5 @@
 import React, { Component, PropTypes } from 'react';
 import { Provider } from 'react-redux';
-import io from 'socket.io-client'
-let agent = require('superagent-promise')(require('superagent'), Promise);
-let socket = io.connect('http://localhost:3000', {reconnect: true});
 
 export default class Root extends Component {
 
@@ -19,22 +16,9 @@ export default class Root extends Component {
   }
 
   componentWillMount() {
-    console.log('mounting...')
     let self = this;
-    // socket.on('chrome-disabled',function(){
-    //   console.log('Now disabled');
-    //   console.log('socket changing status to true');
-    //   self.setState({isDisabled: true})
-    // });
-    // socket.on('chrome-enabled',function(){
-    //   console.log('Now enabled');
-    //   console.log('socket changing status to false');
-    //   self.setState({isDisabled: false})
-    // });
-
     chrome.storage.onChanged.addListener((changes, areaName) => {
       if (areaName === 'local' && changes.hasOwnProperty('isDisabled')) {
-        console.log('onChanged listener setting state: ', changes.isDisabled.newValue);
         this.setState({ isDisabled: changes.isDisabled.newValue });
       }
     })
@@ -48,14 +32,12 @@ export default class Root extends Component {
   }
 
   enable() {
-    // socket.emit('chrome-enable');
     chrome.runtime.sendMessage({ action: 'enable'})
     console.log('enabled click');
     chrome.storage.local.set({ isDisabled: false });
   }
 
   disable() {
-    // socket.emit('chrome-disable');
     chrome.runtime.sendMessage({ action: 'disable'})
     chrome.storage.local.set({ isDisabled: true });
   }
@@ -63,7 +45,6 @@ export default class Root extends Component {
   render() {
     const { store } = this.props;
     let isDisabled;
-    console.log('STATE' , this.state);
     if (this.state) {
       isDisabled = this.state.isDisabled ? 'disabled' : 'enabled';
     }
