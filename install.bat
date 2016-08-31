0ab4eee00c00df4fd674418a0385fafc8b9a2ce8
Eric Ogden <eric.m.ogden@gmail.com>
2016-08-30 21:17:44 -0700

    installation batch file

---
+++ install.bat
@@ -0,0 +1,13 @@
+cd client
+call npm install
+cd ..\server
+call npm install
+cd ..\plugins\atom
+call npm i
+call apm link
+cd ..\schrome
+call npm i
+cd ..\sherpa-view
+call npm i
+call apm link
+echo Installation complete.
